3f090af6e3d9015da0846a9ab86fb52cd029e1f2
dhruv-0987 <dhruvmat1998@gmail.com>
2023-10-09 23:12:55 +1100

    removed maps

--- plannr_UI/src/Pages/Community.js
+++ plannr_UI/src/Pages/Community.js
@@ -1,13 +1,12 @@
 import React from "react";
-import MapComponent from "../Components/Map";
 
 function Community() {
   return (
     <div>
-      <MapComponent />
-
       <div>
-        <p className="text-4xl m-4 p-4 text-brand-green font-playfair">🍎 Dive into Food & Health Events! 🥦</p>
+        <p className="text-4xl m-4 p-4 text-brand-green font-playfair">
+          🍎 Dive into Food & Health Events! 🥦
+        </p>
         <p>
           {" "}
           Host a nutritious workshop, share a recipe, or start a fitness
